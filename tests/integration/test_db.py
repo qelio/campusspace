f90b10bede7eb7637791d677f56e8ad3b2c51058
qelio <slava.samokhvalov@mail.ru>
2025-12-12 06:16:06 +0300

    add tests

--- tests/integration/test_db.py
+++ tests/integration/test_db.py
@@ -3,17 +3,17 @@ import pytest
 class TestDatabaseIntegration:
     """Интеграционные тесты базы данных"""
     
-    def test_db_connection(self, app):
+    def test_db_connection(self, client):
         assert 1 + 1 == 2
     
-    def test_buildings_crud(self, app):
+    def test_buildings_crud(self, client):
         assert 1 + 1 == 2
     
-    def test_rooms_crud(self, app):
+    def test_rooms_crud(self, client):
         assert 1 + 1 == 2
     
-    def test_foreign_key_constraints(self, app):
+    def test_foreign_key_constraints(self, client):
         assert 1 + 1 == 2
     
-    def test_transaction_rollback(self, app):
+    def test_transaction_rollback(self, client):
         assert 1 + 1 == 2
