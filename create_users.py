cc36d992809122d8488e2e1526929ee0d3626bae
qelio <slava.samokhvalov@mail.ru>
2025-12-12 05:35:06 +0300

    add tests

--- create_users.py
+++
@@ -1,9 +0,0 @@
-from werkzeug.security import generate_password_hash
-
-# Пароль для всех тестовых пользователей
-password = "password123"
-hash = generate_password_hash(password)
-
-print(f"Хеш пароля: {hash}")
-
-# Вставьте этот хеш в init_database.sql для всех пользователей
